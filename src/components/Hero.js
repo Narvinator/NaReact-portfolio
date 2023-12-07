e86355c2be2fe89e4fd0eb6c343277bb98a6adca
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-12-07 08:48:15 -0500

    NavBar updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero = () => {
         <div className='hero' id='home'>
             <h2 className='title'>Narvin Blas</h2>
             <h3 className='tagline'>Web Developer</h3>
-            <p>Greetings! I am Narvin, a recent UCF Bootcamp graduate. I have a great deal of passions, but passion for coding created this site. I'm always itching to learn new things and be better and I can't wait to gain more coding experience! 🙌  </p>
+            <p children className = 'paragraph'>Greetings! I am Narvin, a recent UCF Bootcamp graduate. I have a great deal of passions, but passion for coding created this site. I'm always itching to learn new things and be better and I can't wait to gain more coding experience! 🙌  </p>
             <div className="hero-btns">
                 <button className="hire-me">Resume</button>
                 <button className="lets-talk">Comments for me?</button>
