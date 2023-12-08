77b1b9ed1957a6460ec378389375bcd8303dcbe7
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-12-07 20:26:27 -0500

    move files to separate locations for functionality

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -5,7 +5,7 @@ function Hero = () => {
 
     return (
         <div className='hero' id='home'>
-            <h2 className='title'>Narvin Blas</h2>
+            <h2 className='description'>Narvin Blas</h2>
             <h3 className='tagline'>Web Developer</h3>
             <p children className = 'paragraph'>Greetings! I am Narvin, a recent UCF Bootcamp graduate. I have a great deal of passions, but passion for coding created this site. I'm always itching to learn new things and be better and I can't wait to gain more coding experience! 🙌  </p>
             <div className="hero-btns">
@@ -14,15 +14,16 @@ function Hero = () => {
             </div>
             <div className="social-icons">
                 <a href="https://www.linkedin.com/" target="_blank">
-                    <i class="fa-brands fa-linkedin-in"></i>
+                    <i className="fa-brands fa-linkedin-in"></i>
                 </a>
                 
                 </a>
                 <a href="https://github.com/Narvinator" target="_blank">
-                    <i class="fa-brands fa-github"></i>
+                    <i className="fa-brands fa-github"></i>
                 </a>
             </div>
         </div>
+        
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import About from "../components/About";
 import Footer from "../components/Footer";
-import Hero from "../components/Hero";
+import Hero from "./Hero";
 import Projects from "../components/Projects";
 import './Home.css'
 
