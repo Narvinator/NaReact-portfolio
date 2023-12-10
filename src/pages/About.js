f9c49300fb278e2285513a32b09810896e564c26
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-12-09 22:30:38 -0500

    updated front page

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,70 +9,125 @@ const About = () => {
     <section className='about' id='about'>
       <div className="about-me">
         <h2 className='title'>About Me</h2>
-        <p className='about-title'>
+        <p className='paragraph about-para'>
 
-        Greetings! I am Narvin, a recent UCF Bootcamp graduate. I have a great deal of passions, but passion for coding created this site. I'm always itching to learn new things and be better and I can't wait to gain more coding experience! 🙌
+          Greetings! I am Narvin, a recent UCF Bootcamp graduate. I have a great deal of passions, but passion for coding created this site. I'm always itching to learn new things and be better and I can't wait to gain more coding experience! 🙌
         </p>
 
-     
+        <button className="hire-me">
+          <a href="" target="_blank" rel="noreferrer"
+          >
+            RESUME
+          </a>
+        </button>
+      </div>
       <div className="about-skills">
-        <h2 className='skill-title'>Front-End</h2>
+
+
+        <h2 className="skill-title">Front-End</h2>
         <div className="skills">
           <div className="skill">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207"
-              alt="" />
+            <img src={ReactIcon} alt="" />
             <p>React</p>
           </div>
           <div className="skill">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png"
-              alt="" />
+            <img src={JavaScript} alt="" />
             <p>JavaScript</p>
           </div>
-         
+
           <div className="skill">
-            <img src={HTML}
-              alt="" />
+            <img src={HTML} alt="" />
             <p>HTML5</p>
           </div>
           <div className="skill">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/640px-CSS3_logo.svg.png"
-              alt="" />
+            <img src={CSS} alt="" />
             <p>CSS3</p>
           </div>
           <div className="skill">
-            <img src={jQuery}
-              alt="" />
+            <img src={jQuery} alt="" />
             <p>jQuery</p>
           </div>
+
           <div className="skill">
-            
-          </div>
-          <div className="skill">
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/640px-Bootstrap_logo.svg.png'
-              alt="" />
+            <img src={BootStrap} alt="" />
             <p>Bootstrap</p>
           </div>
+
         </div>
-        <h2 className='skill-title'>Back-End</h2>
+        <h2 className="skill-title">Back-End</h2>
         <div className="skills">
           
-        </div>
-       
-       
-       
-        
-         
-          </div>
           <div className="skill">
-          <img src="https://static-00.iconduck.com/assets.00/npm-icon-2048x2048-3xqnd60o.png" alt="" />
-            <p>npm</p>
+              <img src={MySql} alt="" />
+              <p>Node.js</p>
+            </div>
+
+            <div className="skill">
+              <img src={MySql} alt="" />
+              <p>Express.js</p>
+            </div>
+
+            <div className="skill">
+              <img src={MySql} alt="" />
+              <p>MySQL</p>
+            </div>
+
+            <div className="skill">
+              <img src={MySql} alt="" />
+              <p>MongoDB</p>
+            </div>
+
           </div>
+
+          <h2 className="skill-title">Version Control & Deployment</h2>
+          <div className="skills">
+
+            <div className="skill">
+              <img src={GitHublogo} alt="" />
+              <p>GitHub</p>
+            </div>
+
+
+            <div className="skill">
+              <img src={Netlify} alt="" />
+              <p>Netlify</p>
+            </div>
+            <div className="skill">
+              <img src={Heroku} alt="" />
+              <p>Heroku</p>
+            </div>
+          </div>
+          <h2 className="skill-title">Tools</h2>
+          <div className="skills">
+            <div className="skill">
+              <img src={VSCode} alt="" />
+              <p>VS Code</p>
+            </div>
+
+            <div className="skill">
+              <img src={NPMlogo} alt="" />
+              <p>npm</p>
+            </div>
+
+          </div>
+
+          <h2 className="skill-title">Communication</h2>
+          <div className="skills">
+            <div className="skill">
+              <img src={Discord} alt="" />
+              <p>Discord</p>
+            </div>
+
+            <div className="skill">
+              <img src={Zoomlogo} alt="" />
+              <p>Zoom</p>
+            </div>
+          </div>
+
+
+
         </div>
-      </div>
-
-
-
-    </div>
+        
     </section>
   )
 }
